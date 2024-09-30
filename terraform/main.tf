3940dd6f7e5075d359c6d4011c46c77c67d93056
Ivan <filatof@gmail.com>
2024-09-30 23:43:08 +0300

    add traefik revers proxy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,25 @@
-#создает одну ВМ
 terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
+      version = "0.127.0"
     }
   }
   required_version = ">= 0.13"
 
-
-#---------загрузка файла состояний в s3-------------
   backend "s3" {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
     bucket = "terraform.st"
     region = "ru-central1"
-    key    = "one-server.tfstate"
-    shared_credentials_files = [ "di_storage.key" ] #ссылка на ключ доступа к бакету
+    key    = "final-work.tfstate"
+    shared_credentials_files = [ "di_storage.key" ]
 
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
-    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
-
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
   }
 }
 
@@ -33,46 +30,37 @@ provider "yandex" {
   zone                     = "ru-central1-a"
 }
 
-resource "yandex_vpc_network" "infra" {
-  name = "EQ-infra"
+resource "yandex_vpc_network" "infra-vpc" {
+  name = "infra"
 }
 
-resource "yandex_vpc_subnet" "subnet-infra" {
+resource "yandex_vpc_subnet" "subnet" {
   v4_cidr_blocks = ["10.2.0.0/16"]
-  network_id     = yandex_vpc_network.infra.id
+  network_id     = yandex_vpc_network.infra-vpc.id
 }
-
-#для статического адреса
-#resource "yandex_vpc_address" "addr" {
-#  name = "staticAddress"
-#  external_ipv4_address {
-#    zone_id = "ru-central1-a"
-#  }
-#}
 
 resource "yandex_compute_disk" "boot-disk" {
-  name     = "disk-vm1"
+  count    = 2
+  name     = "disk-vm${count.index + 1}"
   type     = "network-hdd"
   size     = 10
-  image_id = "fd87j6d92jlrbjqbl32q"
-
+  image_id = "fd8hglaneh113l00tv83" # ubuntu 22.04 + osLogin 
   labels = {
     environment = "vm-env-labels"
   }
 }
 
-#группы безопасности
 resource "yandex_vpc_security_group" "group1" {
   name        = "my-security-group"
   description = "description for my security group"
-  network_id  = yandex_vpc_network.infra.id
+  network_id  = yandex_vpc_network.infra-vpc.id
 
   labels = {
     my-label = "my-label-value"
   }
 
   dynamic "ingress" {
-    for_each = ["22", "80", "443", "3000", "3100", "9090", "9080", "9095", "9100", "9113", "9104" ]
+    for_each = ["22", "80", "443", "2376", "2377", "3306", "8080", "3000", "3100", "9090", "9080", "9093", "9095", "9100", "9113", "9104" ]
     content {
       protocol       = "TCP"
       description    = "rule1 description"
@@ -91,13 +79,13 @@ resource "yandex_vpc_security_group" "group1" {
   }
 }
 
-
-
-resource "yandex_compute_instance" "vm" {
-  name        = "instance"
+#-----------Claster-NODE-----------------
+resource "yandex_compute_instance" "node" {
+  count =2
+  name        = "node-${count.index + 1}"
   platform_id = "standard-v3"
   zone        = "ru-central1-a"
-  hostname = "instance"
+  hostname = "node-${count.index + 1}"
 
   resources {
     cores         = 2
@@ -106,50 +94,50 @@ resource "yandex_compute_instance" "vm" {
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.boot-disk.id
+    disk_id = yandex_compute_disk.boot-disk[count.index].id
   }
 
   network_interface {
     index     = 1
-    subnet_id = yandex_vpc_subnet.subnet-infra.id
+    subnet_id = yandex_vpc_subnet.subnet.id
     nat       = true
-    #для статического адреса 
-    #nat_ip_address = yandex_vpc_address.addr.external_ipv4_address.0.address
   }
 
   metadata = {
-    #ssh-keys = "fill:${file("~/.ssh/id_ed25519.pub")}"
+    #enable-oslogin = true 
     user-data = "${file("metafile.yaml")}"
+    #ssh-keys = "my-user:${yandex_organizationmanager_user_ssh_key.my_user_ssh_key.data}"
   }
 }
 
-#два варианта вывода ip 
-#для статического адреса
-#output "external_ip" {
-#    value = yandex_vpc_address.addr.external_ipv4_address.0.address
-#}
 
 resource "yandex_dns_zone" "example_zone" {
   name        = "infrastruct"
   description = "my zone dns"
-
   labels = {
     label1 = "lable_zone_dns"
   }
-
   zone    = "infrastruct.ru."
   public  = true
 }
 
-resource "yandex_dns_recordset" "vm-test" {
+resource "yandex_dns_recordset" "node1" {
   zone_id = yandex_dns_zone.example_zone.id
-  name    = "ldap.infrastruct.ru."
+  name    = "node1.infrastruct.ru."
   type    = "A"
   ttl     = 300
   
-  data = [yandex_compute_instance.vm.network_interface.0.nat_ip_address]
+  data = [yandex_compute_instance.node[0].network_interface[0].nat_ip_address]
+}
+resource "yandex_dns_recordset" "node2" {
+  zone_id = yandex_dns_zone.example_zone.id
+  name    = "node2.infrastruct.ru."
+  type    = "A"
+  ttl     = 300
+  
+  data = [yandex_compute_instance.node[1].network_interface[0].nat_ip_address]
 }
 
-output "instance_ip" {
-  value = yandex_compute_instance.vm.network_interface.0.nat_ip_address
+output "node_ip" {
+  value = [for instance in yandex_compute_instance.node : instance.network_interface[0].nat_ip_address]
 }
